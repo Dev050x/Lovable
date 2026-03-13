62e617db548a225b20dee22811f2ee10b66c46ec
0xDev05 <divpatel1274@gmail.com>
2026-03-14 00:10:50 +0530

    ui(prviewbar): add preview bar for mobile in chatpanel

--- frontend/app/components/ChatPanel.tsx
+++ frontend/app/components/ChatPanel.tsx
@@ -2,6 +2,8 @@
 import InputField from "@/app/components/InputField";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { ArrowRight, ExternalLink, Globe } from "lucide-react";
 
 type ChatItem = { from: string, content: string };
 
@@ -19,16 +21,30 @@ export default function ChatPanel({ chatHistory, input, setInput, isLoading, onS
         <div className="flex flex-col h-full w-full p-2 overflow-hidden bg-[#000000]" style={{ height: '100dvh' }}   >
             {/* mobile only top bar */}
             <div className="flex items-center justify-between mb-2 md:hidden">
-                <span className="font-semibold text-sm">Chat</span>
-                {/* <button
-                    onClick={onPreviewClick}
-                    className="relative inline-flex items-center justify-center h-7 rounded-md text-sm text-[#faf9f5] transition-all duration-200 ease-in-out overflow-hidden hover:bg-white/5"
-                >
-                    Preview →
-                </button> */}
-                <Button className="bg-white text-black hover:bg-white/90 text-sm h-8 px-3 rounded-md font-medium">
-                    Get Started
-                </Button>
+                <span className="font-semibold text-xl">CraftAi</span>
+                <div className="flex gap-2">
+                    <Button onClick={onPreviewClick} className="inline-flex items-center justify-center h-7 text-sm border-[0.5px] border-[#908F8E] w-7 px-0">
+                        <Globe size={14} />
+                    </Button>
+
+                    <div className="flex items-center gap-2">
+                        <SignedOut>
+                            <SignInButton mode="modal">
+                                <Button variant="ghost" className="text-white hover:text-black hover:bg-white border text-sm h-8 px-3">
+                                    Login
+                                </Button>
+                            </SignInButton>
+                            <SignInButton mode="modal">
+                                <Button className="bg-white text-black hover:bg-white/90 text-sm h-8 px-3 rounded-md font-medium">
+                                    Get Started
+                                </Button>
+                            </SignInButton>
+                        </SignedOut>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                    </div>
+                </div>
 
             </div>
 
